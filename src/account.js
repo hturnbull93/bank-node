d10b024c3e0c1777e04936767624868b8118f8b9
Harry Turnbull <hturnbull@live.co.uk>
2020-05-20 13:00:55 +0100

    G: deposit calls for new Transaction

--- src/account.js
+++ src/account.js
@@ -1,18 +1,18 @@
-const money = require('./money');
-const Transaction = require('./transaction');
+const money = require("./money");
+const Transaction = require("./transaction");
 
 class Account {
   constructor(transactionClass = Transaction) {
     const STARTING_BALANCE = 0;
 
     this.balance = STARTING_BALANCE;
-    this.transactionClass = transactionClass
+    this.transactionClass = transactionClass;
   }
 
   deposit(amount) {
     let credit = money.pence(amount);
     this.balance += credit;
-    let deposit = new this.transactionClass({credit, balance: this.balance})
+    let deposit = new this.transactionClass({ credit, balance: this.balance });
     let creditDisplay = money.pounds(credit);
     let balanceDisplay = money.pounds(this.balance);
     return `${creditDisplay} deposited. Current balance: ${balanceDisplay}`;
